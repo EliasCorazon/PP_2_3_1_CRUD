4659fed55b22327e17b28bd3a0613d37998bbefd
EliasCorazon <102911055+EliasCorazon@users.noreply.github.com>
2022-06-24 18:24:14 +0500

    corrected

--- src/main/java/project/service/UserServiceImp.java
+++ src/main/java/project/service/UserServiceImp.java
@@ -14,7 +14,7 @@ public class UserServiceImp implements UserService{
     private final UserDao userDao;
 
     @Autowired
-    public UserServiceImp(@Qualifier("userDaoImp") UserDao userDao) {
+    public UserServiceImp(UserDao userDao) {
         this.userDao = userDao;
     }
 
